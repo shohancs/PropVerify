6dcd3e4c35daf4ac0cf877c3a07138b6423388f7
Shohanur Rahman Shohan <shohanurrahmanshohan.cs@gmail.com>
2025-11-22 13:49:21 +0600

    Project Ending

--- admin/inc/db.php
+++ admin/inc/db.php
@@ -2,6 +2,7 @@
 	$db = mysqli_connect( "localhost", "root", "", "property_rental" );
 
 	if ( $db )  {
+		mysqli_set_charset($db, "utf8mb4");
 		// echo "Database Connection Successfully";
 	}
 	else {
